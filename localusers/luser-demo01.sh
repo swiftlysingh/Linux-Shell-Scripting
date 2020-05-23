e83d7c59b0011b010a52a3d247a25af5daded8f8
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-05-23 19:21:05 +0530

    Initial Commit

---
+++ localusers/luser-demo01.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script displays various information to the screen
+# Display "Hello"
+
+echo "Hello"
+echo "${WORD} HeLOOO"
+WORD='script'
+echo '$WORD'
+
+echo "This is a shell ${WORD}ing"
+echo "This is a shell ${WORD}ing"
+
+
